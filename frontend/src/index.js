693a4d1542403280add0ee51410ee60a765cc0ec
vlad <vl.hutsal@gmail.com>
2020-11-21 00:21:11 +0200

    add frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GroupsList from './App'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GroupsList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+

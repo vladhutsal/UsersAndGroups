7735f6bb35b7835212887c6b80ab58a18f0c70f1
vlad <vl.hutsal@gmail.com>
2020-11-21 20:56:33 +0200

    rename: handleRequest() -> submitNewGroup() update: submitNewGroup method now updates parrent Groups components state, after new group being added to DB

--- frontend/src/components/CreateGroup.js
+++ frontend/src/components/CreateGroup.js
@@ -10,7 +10,7 @@ class CreateGroup extends React.Component {
       description: ''
     }
 
-    this.handleRequest = this.handleRequest.bind(this);
+    this.submitNewGroup = this.submitNewGroup.bind(this);
     this.updateField = this.updateField.bind(this)
   }
 
@@ -21,7 +21,7 @@ class CreateGroup extends React.Component {
     })
   }
 
-  async handleRequest(event) {
+  async submitNewGroup(event) {
     event.preventDefault();
     const csrftoken = Cookies.get('csrftoken');
     const data = {
@@ -32,21 +32,22 @@ class CreateGroup extends React.Component {
         'X-CSRFToken': csrftoken
       }
     };
-    console.log(data.body)
     const res = await fetch('http://127.0.0.1:8000/api/groups/create', data);
-    console.log(res);
+    const newGroup = await res.json();
+    console.warn('NEW GROUP');
+    console.log(newGroup);
+    this.props.addNewGroup(newGroup);
   }
 
   
-
   render() {
 
     return (
       <div>
-        <form id='groupForm' onSubmit={this.handleRequest}>
+        <form id='groupForm' onSubmit={ this.submitNewGroup }>
           <input type='text' placeholder='name' name='name' onChange={ this.updateField } value={ this.state.name } />
           <input type='text' placeholder='description' name='description' onChange={ this.updateField } value={ this.state.description }/>
-          <button className='btn btn-warning' type='submit'>submit</button>
+          <button className='btn btn-warning btn-sm' type='submit'>submit</button>
         </form>
       </div>
     )

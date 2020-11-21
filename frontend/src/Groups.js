da035abb9717d5880cb2316ec0198d1842e9def8
vlad <vl.hutsal@gmail.com>
2020-11-21 20:54:23 +0200

    add: addNewGroup method. Passing to CreateGroup component for adding created group to groupsList state

--- frontend/src/Groups.js
+++ frontend/src/Groups.js
@@ -9,13 +9,16 @@ export default class Groups extends React.Component {
         this.state = {
             groupsList: []
         }
+
         this.handleRequest = this.handleRequest.bind(this)
+        this.addNewGroup = this.addNewGroup.bind(this)
     }
 
     async handleRequest() {
         const res = await fetch('http://127.0.0.1:8000/api/groups');
         const data = await res.json();
-        console.log(`GET Groups: ${data}`)
+        console.warn('GET FROM SERVER')
+        console.log(data)
         return data;        
     }
 
@@ -24,13 +27,19 @@ export default class Groups extends React.Component {
         this.setState({
             groupsList: data
         })
-        console.log(`groups component: ${this.state.groupsList}`)
+    }
+
+    addNewGroup(group) {
+        this.setState(state => ({
+            groupsList: [group, ...state.groupsList]  
+        }));
+        console.log(`addNEW: ${this.state.groupsList}`)
     }
 
     render() {
         return (
             <div>
-              <CreateGroup />
+              <CreateGroup addNewGroup={ this.addNewGroup }/>
               <GroupsList groupsList={ this.state.groupsList }/>
             </div>
         )

8727a9755b79b85fdf48ebc1c9b19402c22c190b
vlad <vl.hutsal@gmail.com>
2020-12-03 01:28:29 +0200

    remove: UserCreationSerializer - there is no need to have two diffrent serializers anymore

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,23 +1,10 @@
 from rest_framework import serializers
 from .models import User
-from groups.models import Group
-
-
-class CreateUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ('name', 'group')
+# from groups.models import Group
 
 
 class UserSerializer(serializers.ModelSerializer):
-    group_name = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('name', 'created', 'group', 'group_name')
-
-    def get_group_name(self, usr_instance):
-        group_id = usr_instance.group
-        group = Group.objects.get(id=group_id)
-        return group.name
+        fields = ('id', 'name', 'created', 'group')

87f170c09a15f83c8f0203611bcc7798a713eea4
vlad <vl.hutsal@gmail.com>
2020-12-03 01:29:47 +0200

    add: next views and paths to urls.py: - create_user - delete_user - edit_user

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.users_list),
-
+    path('create', views.create_user),
+    path('delete/<int:id>', views.delete_user),
+    path('edit/<int:id>', views.edit_user)
 ]

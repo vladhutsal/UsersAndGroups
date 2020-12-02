ae8ccfa4daebe957daafc83f43b252c0372817a0
vlad <vl.hutsal@gmail.com>
2020-12-03 01:41:56 +0200

    update: if row isn`t beeing edited, render() method returns static fields, instead of maping (like on GroupRow) add: select element to edit mode -> list all groups except current, which is chosen as default value add: groupsIdToName prop -> are passed from parent RowsHandler and used to find and filter all available groups, when editing the user

--- frontend/src/components/Rows/UserRow.js
+++ frontend/src/components/Rows/UserRow.js
@@ -6,19 +6,17 @@ export default class UserRow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: props.object.id,
-      name: props.object.name,
-      created: props.object.created,
-      group: props.object.group,
-      groupName: props.object.group_name,
-      groups: props.groups
+      name: props.user.name,
+      groupId: props.user.group,
+      groupIdToName: props.groupIdToName
+
     }
     this.updateField = this.updateField.bind(this);
     this.collectData = this.collectData.bind(this);
   }
 
   updateField(event) {
-    const { target: {name, value} } = event;
+    const { target: { name, value } } = event;
     this.setState({
       [name]: value
     });
@@ -27,25 +25,30 @@ export default class UserRow extends React.Component {
   async collectData() {
     const data = {
       name: this.state.name,
-      description: this.state.descr
+      group: this.state.groupId
     };
-    const res = await this.props.saveRow(data, this.state.id);
+    await this.props.saveRow(data, this.props.user.id);
     this.props.editRow();
+    return;
   }
 
   render() {
-    const object = this.props.object;
-    const objKeys = Object.keys(object);
-    const objId = this.state.id;
+    const user = this.props.user;
+    const userId = this.props.user.id;
+    const groupsIdToName = this.state.groupIdToName;
+    let groupKeys = Object.keys(this.state.groupIdToName);
+    groupKeys = groupKeys.filter(val => {return parseInt(val) !== this.state.groupId});
+
     if (!this.props.isEdit) {
       return (
-        <tr key={object.name}>
-          { objKeys.map(fieldName => (
-            <td key={fieldName}>{object[fieldName]}</td>
-          ))}
+        <tr key={this.state.name}>
+          <td>{userId}</td>
+          <td>{user.name}</td>
+          <td>{user.created}</td>
+          <td>{this.props.groupIdToName[this.state.groupId]}</td>
           <td>
             <Actions
-              id={objId}
+              id={userId}
               edit={this.props.editRow}
               isEdit={this.props.isEdit}
               delete={this.props.deleteRow}
@@ -55,26 +58,31 @@ export default class UserRow extends React.Component {
       )
     }
 
-    else {
-      return(
-      <tr>
-        <td>{objId}</td>
-        <td>
-          <input className='form-control' name='name'
-            value={this.state.name}
-            onChange={this.updateField} />
-        </td>
-        <td>
-          <input className='form-control' name='descr'
-            value={this.state.descr}
-            onChange={this.updateField} />
-        </td>
-        <td><Actions
-          id={this.state.id}
-          edit={this.props.editRow}
-          isEdit={this.props.isEdit}
-          save={this.collectData} /></td>
-      </tr>
-    )}
+    else if (this.props.isEdit) {
+      return (
+        <tr>
+          <td>{userId}</td>
+          <td>
+            <input className='form-control' name='name'
+              value={this.state.name}
+              onChange={this.updateField} />
+          </td>
+          <td>{user.created}</td>
+          <td>
+            <select name='groupId' onChange={this.updateField}>
+              <option disabled selected value={this.state.groupId}>{groupsIdToName[this.state.groupId]}</option>
+              {groupKeys.map(groupId => (
+                <option key={groupId} value={groupId}>{groupsIdToName[groupId]}</option>
+              ))}
+            </select>
+          </td>
+            <td><Actions
+              id={this.props.user.id}
+              edit={this.props.editRow}
+              isEdit={this.props.isEdit}
+              save={this.collectData} /></td>
+        </tr>
+      )
+    }
   }
 }

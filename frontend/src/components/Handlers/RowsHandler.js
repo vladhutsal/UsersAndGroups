883fca21992625be0bf0322666a47e005555d98f
vlad <vl.hutsal@gmail.com>
2020-12-04 21:19:14 +0200

    now RowsHandler renders rows, instead of being rendered with each row update: render method now returning GroupRow, UserRow or a message about empty table move: editRow method and edit state are moved to childs: GroupRow and UserRow add: objects state now stores all objects fetched from database (fetchedObjects state of Table component) add: componentDidUpdate method tracks changes of fetchedObjects from parent Table component and update state respectively update: deleteRow method now uses id as an argument update: saveEditedRow now uses id as an argument

---
+++ frontend/src/components/Handlers/RowsHandler.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import GroupRow from '../Rows/GroupRow';
+import UserRow from '../Rows/UserRow';
+
+
+export default class RowsHandler extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      objects: []
+    }
+    this.deleteRow = this.deleteRow.bind(this);
+    this.saveEditedRow = this.saveEditedRow.bind(this);
+    this.deleteRow = this.deleteRow.bind(this);
+  }
+  
+
+  componentDidUpdate(prevProps) {
+    if (this.props.objects !== prevProps.objects) {
+      this.setState({
+        objects: this.props.objects
+      })
+    }
+  }
+
+
+  async deleteRow(id) {
+    const url = `${this.props.url}/delete/${id}`
+    const res = await this.props.handleRequest(url, 'DELETE');
+    console.log(res.message);
+    res.data.id = id;
+    this.props.updateFetchedObjects('delete', res.data);
+    return;
+  }
+
+
+  async saveEditedRow(data, id) {
+    const url = `${this.props.url}/edit/${id}`;
+    const res = await this.props.handleRequest(url, 'POST', data);
+    this.props.updateFetchedObjects('edit', res.data)
+    return;
+  }
+
+  render() {
+    if (this.props.mode === 'groups' && this.props.objects.length > 0) {
+      return (
+        <>
+          {this.state.objects.map(object => (
+            <GroupRow group={object}
+              key={object.id}
+              saveRow={this.saveEditedRow}
+              deleteRow={this.deleteRow}/>
+          ))}
+        </>
+      );
+    }
+    else if (this.props.mode === 'users' && this.props.objects.length > 0) {
+      return (
+        <>
+          {this.props.objects.map(object => (
+            <UserRow user={object}
+              key={object.id}
+              groupsIdToName={this.props.groupsIdToName}
+              saveEditedRow={this.saveEditedRow}
+              deleteRow={this.deleteRow}/>
+          ))}
+        </>
+      );
+    }
+
+    else {
+      return (
+        <tr>
+          <td>Sorry, there is no data. Add something to see the table</td>
+        </tr>
+      );
+    }
+
+  }
+}

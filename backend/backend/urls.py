83cbae43db30c20280d1431f1aaf5a71f00e0b9b
vlhutsal <vl.hutsal@gmail.com>
2021-01-14 15:03:39 +0200

    comented old urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,6 @@ from django.urls import path, include
 # from users_and_groups import views_group
 
 urlpatterns = [
-    # path('', views_group.index),
-    # path('groups/', views_group.index),
-    # path('users/', views_group.index),
     path('api/', include('users_and_groups.urls')),
     path('admin/', admin.site.urls),
 ]
